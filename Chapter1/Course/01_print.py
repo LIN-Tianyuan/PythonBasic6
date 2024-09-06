1620efd61b98fe19e7fb3dc756549e1ae3ff0288
LIN-Tianyuan <47159399+LIN-Tianyuan@users.noreply.github.com>
2024-09-06 09:18:01 +0200

    week 1

---
+++ Chapter1/Course/01_print.py
@@ -0,0 +1,8 @@
+print("Today is a good day!")
+print("j'aime les pommes, il aime les poires.")
+
+print("----------")
+print("j'aime les pommes,")
+print("il aime les poires.")
+print("----------")
+print("J'aime les pommes, \nil aime les poires.")
